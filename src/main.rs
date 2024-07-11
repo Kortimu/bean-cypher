531c217f6b0533637f089d5ea2e23c8bb3bcbe2a
Kortimu <mareks.greizans05@gmail.com>
2024-07-12 00:53:33 +0300

    fix: replace incorrect app id

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_icon(
-            eframe::icon_data::from_png_bytes(&include_bytes!("../assets/bean.png")[..])
-                .expect("Failed loading the icon."),
-        ),
+        viewport: egui::ViewportBuilder::default()
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/bean.png")[..])
+                    .expect("Failed loading the icon."),
+            )
+            .with_title(format!(
+                "Bean Cypher (Alpha v{}-dev)",
+                env!("CARGO_PKG_VERSION")
+            )),
         ..Default::default()
     };
     eframe::run_native(
-        &format!("Bean Cypher Alpha v{}-dev", env!("CARGO_PKG_VERSION")),
+        "Bean Cypher",
         native_options,
         Box::new(|cc| Ok(Box::new(bean_cypher::BeanCypherApp::new(cc)))),
     )
