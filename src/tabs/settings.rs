4ab6c4808de0deca775a1ce5e360100874c26f5e
Kortimu <mareks.greizans05@gmail.com>
2026-06-20 23:19:17 +0300

    fix: settings and credits look wonky on different screens

--- src/tabs/settings.rs
+++ src/tabs/settings.rs
@@ -1,9 +1,22 @@
 use crate::{toggle, TestApp};
 
 pub fn show_settings(ui: &mut egui::Ui, app: &mut TestApp) {
-    ui.heading("SETTINGS!!!!");
-    ui.horizontal(|ui| {
-        ui.add(toggle(&mut app.set_lowoutput));
-        ui.label("lowercase output");
-    });
+    ui.allocate_ui_with_layout(
+        ui.available_size(),
+        egui::Layout::top_down(egui::Align::Center),
+        |ui| {
+            ui.set_max_width(300.0);
+            
+            ui.horizontal(|ui| {
+                ui.label("lowercase output");
+                ui.allocate_ui_with_layout(
+                    ui.available_size(),
+                    egui::Layout::right_to_left(egui::Align::Center),
+                    |ui| {
+                        ui.add(toggle(&mut app.set_lowoutput));
+                    }
+                );
+            });
+        }
+    );
 }
